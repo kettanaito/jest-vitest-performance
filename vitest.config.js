5dc384d91d3733727defe20a426c0833dc2b86b2
Artem Zakharchenko <kettanaito@gmail.com>
2025-03-03 11:45:32 +0100

    set `isolate` to `false` for vitest

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    cache: false,
+    isolate: false,
     browser: {
       enabled: true,
       provider: 'playwright',
